ae66f605c9fe9727c879d58a2fa6ac868700c4d0
saqibbhat48 <bhatsaqib48@gmail.com>
2025-03-06 17:45:52 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors'); 
 const greetRouter = require('./routes/messageRoute');
 const dotenv = require("dotenv")
+const path = require("path")
 
 const app = express();
 dotenv.config()
@@ -17,6 +18,14 @@ app.use(cors({
 
 
 app.use('/api/greet', greetRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
